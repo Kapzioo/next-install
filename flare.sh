ae6df4c9c21c92432813448ecbe5ca7fdc32377e
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2023-07-26 05:18:14 +0200

    Create flare.sh

---
+++ flare.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+
+sudo dpkg -i cloudflared.deb
+
+sudo cloudflared service install eyJhIjoiZWJiZTE2YWMzZTY4NGNmYjljZTJjZTdkOWNhNTRiMmIiLCJ0IjoiMGE5YTJjNjktZGJlOC00ZDY1LWFiMzUtNThhZDI2MzJjMDYxIiwicyI6Ik1qY3hNRFJoTm1VdE5UZ3hNUzAwTmpRNUxXSTJNall0WVRnNVpqRTNObU15TkdKaiJ9
