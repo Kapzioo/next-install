211d59ac5a98e4a248cf20eb262b4b6f62a1e420
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2023-07-26 04:36:52 +0200

    Delete 5.sh

--- 5.sh
+++
@@ -1,5 +0,0 @@
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
-
-sudo dpkg -i cloudflared.deb
-
-sudo cloudflared service install eyJhIjoiZWJiZTE2YWMzZTY4NGNmYjljZTJjZTdkOWNhNTRiMmIiLCJ0IjoiYjQ1OGQ2NDEtOTMxNC00ZmNhLWIyNzQtNmJhZDNiNmI5NTUyIiwicyI6Ik5qZzBZek14TTJZdE56azRaaTAwWmpRNExUZzVaVFl0WldOaE1URTRORGhpTmpZNCJ9
