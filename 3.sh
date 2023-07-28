58164e02ccb900ab00bd65258e09781d734d09e8
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2023-07-28 22:50:35 +0200

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,3 +1,7 @@
 sudo apt install wget unzip
 cd /tmp/
 wget https://download.nextcloud.com/server/releases/latest.zip
+unzip latest.zip
+sudo mv nextcloud/ /var/www/html/
+sudo chown -R www-data:www-data /var/www/html/nextcloud
+sudo chmod -R 755 /var/www/html/nextcloud
