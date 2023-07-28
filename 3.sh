26dcccd560ecbc2863b5f782ceab2d519faa45fb
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2023-07-28 22:52:01 +0200

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -5,3 +5,5 @@ unzip latest.zip
 sudo mv nextcloud/ /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/nextcloud
 sudo chmod -R 755 /var/www/html/nextcloud
+cd /home/next-install
+cp nextcloud.conf /etc/apache2/conf-enabled/
