2d9fdf24f982182a97a9c6b45ec6d45adb8abfcd
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2023-07-28 22:49:20 +0200

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,9 +1,3 @@
-cd /home/next-install
-cp nextcloud.conf /etc/apache2/sites-available/
-nano /etc/apache2/sites-available/nextcloud.conf
-a2ensite nextcloud.conf
-systemctl restart apache2
-mkdir /home/data
-chown -R www-data:www-data /var/www/nextcloud 
-chmod -R 755 /var/www/nextcloud
-chown –R www-data:www-data /home/data 
+sudo apt install wget unzip
+cd /tmp/
+wget https://download.nextcloud.com/server/releases/latest.zip
