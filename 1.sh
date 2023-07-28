4a617c44bb41d6a68e0e06d0b0128a00cc731e6b
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2023-07-28 22:42:09 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,12 +1,4 @@
-apt update && apt upgrade
-apt install apache2 unzip wget curl mariadb-client mariadb-server nano 
-apt-get install ca-certificates apt-transport-https software-properties-common
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/sury-php.list 
-apt install gnupg gnupg2 gnupg1
-wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add -
-apt update && apt upgrade
-sudo add-apt-repository ppa:ondrej/php
-apt install php8.1
-apt install libapache2-mod-php8.1 php8.1-{zip,xml,mbstring,gd,curl,imagick,intl,bcmath,gmp,cli,mysql,apcu,redis}
-nano /etc/php/8.0/apache2/php.ini  
-sudo systemctl restart mariadb
+sudo apt install apache2 libapache2-mod-php php-cli php-fpm php-json php-intl php-imagick php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath mariadb-server
+sudo ufw allow 80
+sudo ufw allow 443
+sudo mysql_secure_installation
