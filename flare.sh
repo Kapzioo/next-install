f2e981fa6c3c7a7ae4fc40bf8ae42d56e01f1901
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2023-07-29 19:54:16 +0200

    Update flare.sh

--- flare.sh
+++ flare.sh
@@ -1,5 +1,3 @@
 curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
 
 sudo dpkg -i cloudflared.deb && 
-
-sudo cloudflared service install eyJhIjoiZWJiZTE2YWMzZTY4NGNmYjljZTJjZTdkOWNhNTRiMmIiLCJ0IjoiMjM2NjVlYjAtYTU3Mi00ZDJhLWJmMzEtZTg0Mjk5ZTU2ZmFhIiwicyI6IlpEVm1NVGt3Tm1FdE0yWmpNQzAwWldFNExUZ3dNR010WXpJMk9EWTFaVGxqWTJRNCJ9
