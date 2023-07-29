31f359b9fbf179192ff1b2c39fd018ea88aed255
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2023-07-29 16:44:36 +0200

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -3,8 +3,6 @@ cd /tmp/
 wget https://download.nextcloud.com/server/releases/latest.zip
 unzip latest.zip
 sudo mv nextcloud/ /var/www/html/
-sudo chown -R www-data:www-data /var/www/html/nextcloud
-sudo chmod -R 755 /var/www/html/nextcloud
 cd /home/next-install
 cp nextcloud.conf /etc/apache2/conf-enabled/
 sudo a2enmod rewrite dir mime env headers
